84c19cb2dc7e637f60d501f34b264b416a0576fc
MissKluck <sigrid.b.kluck@gmail.com>
2024-06-05 10:25:41 +0200

    created an input element and readied it for collat

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # collatz-conjuncture
 
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/MissKluck/collatz-conjuncture)
+## Algoriitmen
+
+- Start med et valgfritt tall
+- Hvis tallet er et partall
+    - Del tallet på 2 ( n / 2 )
+- Hvis tallet er et oddetall
+    - Gang med 3 og legg til 1 ((n*3)+1)
+- Repeter til tallet er 1
+
+## Oppgave
+
+## Steg 1
+
+- Ta inn en tallverdi via et inpur element
+- Bruk til å kjøre den gjennom Collatz Formodning
+- Print alle tallene til konsollen
+
+## Steg 2
+
+- Oppsummer resultatet og vis det i et HTML element
+
+## Optional
+
+- Gjør kalkulasjonen i en egen tråd
+    [Web Workers](https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers)
+
+## Lenker
+
+- [Collatz Formodning] - (https://no.wikipedia.org/wiki/Collatz%E2%80%99_formodning)
+- [Collatz Conjecture] - (https://en.wikipedia.org/wiki/Collatz_conjecture)
+
+## Javascript
+function handleSubmit(event) {
+    /// Stopper form submit fra å refraehe nettsiden
+    event.preventDefault() 
+    console.log("Hello World!")
+
+}
+/// Arrow function, anonymous function, lambda function
+(event) => {
+    /// Stopper form submit fra å refraehe nettsiden
+    event.preventDefault() 
+    console.log("Hello World!")
+}
--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-console.log('Hello!');
+// Først finn form elementet
+/** Hjelper javascript å forstå at dette skal operere med et HTMLelement og gir da alle relevante kommandoer
+ *  @type {HTMLFormElement}
+ */
+const inputForm = document.getElementById("collatz-input")
+inputForm.addEventListener("submit", (event)=>{
+    event.preventDefault() 
+    
+    const inputElement = document.getElementById("input-number")
+    const input = inputElement.value
+
+    // Sjekk om verdien som er skrevet inn er gyldig ! står for ikke, du kan invertere kode bare ved å legge til et uttropstegn(10 === 20)= false --> !(10 === 20)= true
+    if (!(input > 0)) {
+        return
+    }
+
+    const result = collatz(input) // Prosseser input
+    const newElement = createElement(result) // Lag et nytt resultat element
+    const resultContainer =document.getElementById("result-container") // Hent ut hvor det skal settes inn
+    resultContainer.appendChild(newElement) // Sett inn i dokumentet
+
+    // Reset input feltet
+    // inputElement.value = 0
+    inputForm.reset()
+
+    // console.log("2 should be even: " + isEven(2))
+    // console.log("3 should be odd: " + isEven(3))
+})
+
+/**
+ * Denne funksjonen går igjennom collatz conjecture
+ * med tallet den får
+ * @param {number} number 
+ * @returns 
+ */
+function collatz(number) {
+    // Noe logikk
+    console.log(number)
+
+    const newNumber = number - 1
+
+    if (isEven(number)) {
+        return newNumber
+    } else {
+        // Rekursjon
+        return collatz(newNumber)
+    }
+}
+
+function isEven(number) {
+    return number % 2 === 0
+}
+
+function createElement(number) {
+    const element = document.createElement("li")
+    element.textContent = number
+    return element
+}
